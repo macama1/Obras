f995ad0d4d0f80440e554d275928c0187d98f502
Natstone <mrivas@natstone.cl>
2025-09-15 12:15:44 -0300

    Tipos de Construcción

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ export const estadoObraOptions = [
   'Cierre Perimetral', 'Limpieza y Demolición', 'Instalación de Faena', 'Obras Preliminares', 'Movimiento de Tierra', 'Excavaciones', 'Fundaciones', 'Obra Gruesa -20%', 'Obra Gruesa 20% ~ 50%', 'Obra Gruesa +50%', 'Terminaciones -20%', 'Terminaciones 20% ~ 50%', 'Terminaciones +50%', 'Terminada', 'Detenida', 'No Aplica',
 ];
 export const tipoConstruccionOptions = [
-  "Residencial", "Edificio Departamentos", "Industrial", "Comercial", "Obras Menores", "Educacional", "Casas", 'No Aplica'
+  "Residencial", "Edificio Departamentos", "Industrial", "Comercial", "Obras Menores", "Educacional", "Casas", "Salud", "Culto", "Deportivo", 'No Aplica'
 ];
 export const lesVendemosOptions = ['Sí', 'No'];
 export const regionesYComunas = [
