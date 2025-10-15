268148195b937f1b660c19aa32d7c6102b4d4660
Natstone <mrivas@natstone.cl>
2025-10-15 10:58:49 -0300

    rut
    rut

--- src/App.tsx
+++ src/App.tsx
@@ -442,7 +442,7 @@ export default function App() {
             <div className="form-column" style={{gridColumn: '1 / -1'}}>
               <div className="form-field"><label>Acciones Última Reunion</label><textarea value={obraDetails['Acciones Última Reunion'] || ''} rows={2} disabled></textarea></div>
               <div className="form-field"><label>Comentarios Última Visita</label><textarea name="Comentarios Última Visita" value={obraDetails['Comentarios Última Visita'] || ''} onChange={handleInputChange} rows={4}></textarea></div>
-              <div className="form-field"><label>Rut Empresa</label><textarea name="Rut Empresa" value={obraDetails['Rut Empresa'] || ''} onChange={handleInputChange} rows={4}></textarea></div>
+              <div className="form-field"><label>Rut Empresa</label><textarea name="Rut Empresa" value={obraDetails['Rut Empresa'] || ''} onChange={handleInputChange} rows={1}></textarea></div>
               <div className="form-field"><label>Última Actualización</label><input type="text" value={formatDate(obraDetails['Última Actualización'])} disabled /></div>
             </div>
           </div>
